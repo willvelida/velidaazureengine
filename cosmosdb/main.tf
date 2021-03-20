9a42d7b0eb4eb9b317d104257374db44438fbee9
willvelida <willvelida@hotmail.co.uk>
2021-03-20 16:35:26 +1300

    Creating Cosmos DB account with remote state file!

---
+++ cosmosdb/main.tf
@@ -0,0 +1,38 @@
+terraform {
+    backend "azure" {
+      resource_group_name = "velidarg"
+      storage_account_name = "velidaterraform"
+      container_name = "tstate"
+      key = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name = "willvelidacosmosdb"
+  location = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
+  enable_automatic_failover = true
+  
+  geo_location {
+    location = var.resource_group_location
+    failover_priority = 0
+  }
+
+  analytical_storage_enabled = true
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
---
+++ cosmosdb/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type = string
+  default = "velidaazureengine-rg"
+}
+
+variable "resource_group_location" {
+  description = "Location of the resource group"
+  type = string
+  default = "australiaeast"
+}
