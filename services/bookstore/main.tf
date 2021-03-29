926365ce8046de1b68115a93e13d3e0fc195f77e
willvelida <willvelida@hotmail.co.uk>
2021-03-29 20:47:38 +1300

    Adding API function app terraform

--- services/bookstore/main.tf
+++ services/bookstore/main.tf
@@ -12,6 +12,7 @@ provider "azurerm" {
   features {}
 }
 
+# Creating the resource group for Bookstore
 module "resource_group" {
     source = "../../modules/resource_group"
     resource_group_name = var.resource_group_name
@@ -21,4 +22,39 @@ module "resource_group" {
         "Resource-Specific" = "false"
         "ApplicationName" = "VelidaBookstore"
     }
+}
+
+# Creating the storage account for the Bookstore API
+module "storage_account" {
+    source = "../../modules/storage_account"
+    storage_account_name = var.function_storage_name
+    resource_group_name = module.resource_group.name
+    storage_location = module.resource_group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    account_kind = "StorageV2"
+    is_hns_enabled = "false"
+}
+
+## Creating the app service plan
+resource "azurerm_app_service_plan" "asp" {
+    name = var.app_service_plan_name
+    location = module.resource_group.location
+    resource_group_name = module.resource_group.name
+    kind = "FunctionApp"
+
+    sku {
+        tier = "Dynamic"
+        size = "Y1"
+    }
+}
+
+## Deploying the Bookstore API function app
+resource "azurerm_function_app" "bookstoreapi" {
+    name = var.bookstore_api_function_name
+    location = module.resource_group.location
+    resource_group_name = module.resource_group.name
+    app_service_plan_id = azurerm_app_service_plan.asp.id
+    storage_account_name = module.storage_account.storage_account_name
+    storage_account_access_key = module.storage_account.primary_access_key
 }
--- services/bookstore/variables.tf
+++ services/bookstore/variables.tf
@@ -9,3 +9,21 @@ variable "resource_group_location" {
   description = "The location of the resource group for bookstore"
   type = string
 }
+
+variable "function_storage_name" {
+  default = "savelidabookstoreapi"
+  description = "Name of the storage account for the function app"
+  type = string
+}
+
+variable "app_service_plan_name" {
+  default = "aspvelidabookstore"
+  description = "Name of the App Service Plan for Bookstore"
+  type = string
+}
+
+variable "bookstore_api_function_name" {
+  default = "favelidabookstoreapi"
+  description = "Name of the Function App for Bookstore API"
+  type = string
+}
