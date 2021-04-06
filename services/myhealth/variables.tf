63bc1f5255bb7c5d3833d49e252c68f1fac2578f
willvelida <willvelida@hotmail.co.uk>
2021-04-06 20:16:56 +1200

    Adding services folder for myhealth

---
+++ services/myhealth/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "myhealth-rg"
+  description = "Name of the resource group used for MyHealth"
+  type = string 
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+  description = "Location used for the resource group for MyHealth"
+  type = string
+}
