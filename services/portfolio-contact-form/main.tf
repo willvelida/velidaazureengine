3cfd31bebdd22b399db611d39d3e42e774404d94
willvelida <willvelida@hotmail.co.uk>
2021-04-24 14:27:41 +1200

    Adding function, storage account and kv access policy

--- services/portfolio-contact-form/main.tf
+++ services/portfolio-contact-form/main.tf
@@ -12,6 +12,7 @@ provider "azurerm" {
   features {}
 }
 
+# Create resource group
 module "resource_group" {
     source = "../../modules/resource_group"
     resource_group_name = var.resource_group_name
@@ -20,5 +21,61 @@ module "resource_group" {
         "Terraform" = "true"
         "Resource-Specific" = "false"
         "ApplicationName" = "Portfolio-Site"
+        "Service-Name" = "Portfolio.ContactForm"
     }
+}
+
+# Importing Key Vault
+data "azurerm_key_vault" "velidakeyvault" {
+  name = var.velida_key_vault
+  resource_group_name = var.velida_resource_group_name
+}
+
+# Create App Service Plan for all MyHealth Function Apps
+resource "azurerm_app_service_plan" "portfoliositeplan" {
+  name = var.contactform_app_service_plan_name
+  location = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  kind = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+# Creating the storage account for Portfolio.ContactForm
+module "storage_account" {
+    source = "../../modules/storage_account"
+    storage_account_name = var.function_storage_name
+    resource_group_name = module.resource_group.name
+    storage_location = module.resource_group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    account_kind = "StorageV2"
+    is_hns_enabled = "false"
+}
+
+# Creating the function app for Portfolio.ContactForm
+resource "azurerm_function_app" "portfoliocontactform" {
+  name = var.portfolio_contactform_function_name
+  location = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.portfoliositeplan.id
+  storage_account_name = module.storage_account.storage_account_name
+  storage_account_access_key = module.storage_account.primary_access_key
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  lifecycle {
+    ignore_changes = [app_settings]
+  }
+}
+
+resource "azurerm_key_vault_access_policy" "velidakeyvault_policy" {
+  key_vault_id = data.azurerm_key_vault.velidakeyvault.id
+  tenant_id = azurerm_function_app.portfoliocontactform.identity[0].tenant_id
+  object_id = azurerm_function_app.portfoliocontactform.identity[0].principal_id
+  secret_permissions = [ "get","list" ]
 }
--- services/portfolio-contact-form/variables.tf
+++ services/portfolio-contact-form/variables.tf
@@ -9,3 +9,31 @@ variable "resource_group_location" {
   description = "The location of the resource group for bookstore"
   type = string
 }
+
+variable "velida_key_vault" {
+  default = "willvelidakeyvault"
+}
+
+variable "velida_resource_group_name" {
+  default = "velidaazureengine-rg"
+}
+
+variable "contactform_app_service_plan_name" {
+  default = "aspvelidaportfoliosite"
+}
+
+variable "function_storage_name" {
+  default = "savelidapscontact"
+  description = "Name of the storage account for Portfolio.ContactForm"
+  type = string
+}
+
+variable "portfolio_contactform_function_name" {
+  default = "favelidapscontact"
+  description = "Name of the Portfolio.ContactForm App"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "e60bb76c-8fda-4ed4-a354-4836e3bfcbc3"
+}
