7771d0b2537e7e3ad16cc53ea146ac21a74ac6a8
willvelida <willvelida@hotmail.co.uk>
2021-04-24 20:42:05 +1200

    Removing Exceptions function

--- services/myhealth/main.tf
+++ services/myhealth/main.tf
@@ -45,21 +45,6 @@ data "azurerm_servicebus_namespace" "namespace" {
   resource_group_name = var.velida_generic_resource_group_name
 }
 
-module "myhealth_exception_topic" {
-  source = "../../modules/service_bus_topic"
-  topic_name = var.exception_topic_name
-  topic_resource_group = data.azurerm_servicebus_namespace.namespace.resource_group_name
-  topic_namespace = data.azurerm_servicebus_namespace.namespace.name
-}
-
-resource "azurerm_servicebus_subscription" "sub" {
-  name = var.exception_subscription_name
-  resource_group_name = data.azurerm_servicebus_namespace.namespace.resource_group_name
-  namespace_name = data.azurerm_servicebus_namespace.namespace.name
-  topic_name = var.exception_topic_name
-  max_delivery_count = 1
-}
-
 # Create Database for MyHealth
 resource "azurerm_cosmosdb_sql_database" "db" {
   name = var.myhealth_db_name
