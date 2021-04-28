25e368fb6a7a2e0ab9f4ece5266dff9f59a23a56
willvelida <willvelida@hotmail.co.uk>
2021-04-28 20:38:41 +1200

    Adding service for MyHealth.API.Activity

---
+++ services/myhealth-api-activity/variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  default = "myhealthapiactivity-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "velida_key_vault" {
+  default = "willvelidakeyvault"
+}
+
+variable "velida_resource_group_name" {
+  default = "velidaazureengine-rg"
+}
+
+variable "myhealth_app_service_plan" {
+  default = "myhealthappserviceplan"
+  description = "Name of the App Service Plan for MyHealth"
+  type = string
+}
+
+variable "myhealth_resource_group" {
+  default = "myhealth-rg"
+  description = "The name of the resource group for MyHealth"
+  type = string
+}
+
+variable "function_storage_name" {
+  default = "samyhealthapiactivity"
+  description = "Name of the storage account for MyHealth.API.Activity"
+  type = string
+}
+
+variable "myhealth_dbsink_activity_function_name" {
+  default = "famyhealthapiactivity"
+  description = "Name of the MyHealth.API.Activity App"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "e60bb76c-8fda-4ed4-a354-4836e3bfcbc3"
+}
