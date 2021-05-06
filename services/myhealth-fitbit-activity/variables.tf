6cec114b36237856493f28299788d2e993817947
willvelida <willvelida@hotmail.co.uk>
2021-05-06 18:21:13 +1200

    Adding initial cut for MyHealth.Fitbit.Activity service

---
+++ services/myhealth-fitbit-activity/variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  default = "myhealthfitbitactivity-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "velida_key_vault" {
+  default = "willvelidakeyvault"
+}
+
+variable "velida_resource_group_name" {
+  default = "velidaazureengine-rg"
+}
+
+variable "myhealth_app_service_plan" {
+  default = "myhealthappserviceplan"
+  description = "Name of the App Service Plan for MyHealth"
+  type = string
+}
+
+variable "myhealth_resource_group" {
+  default = "myhealth-rg"
+  description = "The name of the resource group for MyHealth"
+  type = string
+}
+
+variable "function_storage_name" {
+  default = "samyhealthfitbitactivity"
+  description = "Name of the storage account for MyHealth.Fitbit.Sleep"
+  type = string
+}
+
+variable "myhealth_fitbit_sleep_function_name" {
+  default = "famyhealthfitbitactivity"
+  description = "Name of the MyHealth.Fitbit.Sleep App"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "e60bb76c-8fda-4ed4-a354-4836e3bfcbc3"
+}
