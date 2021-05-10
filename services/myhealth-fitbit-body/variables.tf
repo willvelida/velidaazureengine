733f809c4565219badc36334008346c46a93dacb
willvelida <willvelida@hotmail.co.uk>
2021-05-10 20:08:02 +1200

    Adding initial cut for MyHealth.Fitbit.Body

---
+++ services/myhealth-fitbit-body/variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  default = "myhealthfitbitbody-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "velida_key_vault" {
+  default = "willvelidakeyvault"
+}
+
+variable "velida_resource_group_name" {
+  default = "velidaazureengine-rg"
+}
+
+variable "myhealth_app_service_plan" {
+  default = "myhealthappserviceplan"
+  description = "Name of the App Service Plan for MyHealth"
+  type = string
+}
+
+variable "myhealth_resource_group" {
+  default = "myhealth-rg"
+  description = "The name of the resource group for MyHealth"
+  type = string
+}
+
+variable "function_storage_name" {
+  default = "samyhealthfitbitbody"
+  description = "Name of the storage account for MyHealth.Fitbit.Body"
+  type = string
+}
+
+variable "myhealth_fitbit_body_function_name" {
+  default = "famyhealthfitbitbody"
+  description = "Name of the MyHealth.Fitbit.Body App"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "e60bb76c-8fda-4ed4-a354-4836e3bfcbc3"
+}
