5a4386d5f28e2fbcd22f6812327104ec1e059f92
willvelida <willvelida@hotmail.co.uk>
2021-05-25 20:57:38 +1200

    Adding MyHealth APIM service

---
+++ services/myhealth-apim-management/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "azure" {
+      resource_group_name = "velidarg"
+      storage_account_name = "velidaterraform"
+      container_name = "myhealthapimtfstate"
+      key = "terraform.tstate"
+  }
+}
+
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+
+# Import App Insights
+data "azure_application_insights" "appinsights" {
+    name = var.app_insights_name
+    resource_group_name = var.app_insights_resource_group
+}
+
+# Import API Management Instance
+data "azurerm_api_management" "apim" {
+  name = var.api_name
+  resource_group_name = var.api_resource_group_name
+}
+
+# Linked APIM to App Insights
+resource "azurerm_api_management_logger" "apilogger" {
+  name = var.api_logger_name
+  api_management_name = data.azurerm_api_management.apim.name
+  resource_group_name = data.azurerm_api_management.apim.resource_group_name
+
+  application_insights {
+    instrumentation_key = data.azurerm_application_insights.appinsights.application_insights_instrumentation_key
+  }
+}
---
+++ services/myhealth-apim-management/variables.tf
@@ -0,0 +1,19 @@
+variable "app_insights_name" {
+  default = "velidaapplicationinsights"
+}
+
+variable "app_insights_resource_group" {
+  default = "velidaazureengine-rg"
+}
+
+variable "api_name" {
+  default = "myhealthapimportal"
+}
+
+variable "api_resource_group_name" {
+  default = "myhealth-rg"
+}
+
+variable "api_logger_name" {
+  default = "myhealthapimlogger"
+}
