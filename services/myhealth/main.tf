63bef5271fda9c879d2f0018b50a415fbc4b6047
willvelida <willvelida@hotmail.co.uk>
2021-06-15 20:52:25 +1200

    Removing APIM code from MyHealth

--- services/myhealth/main.tf
+++ services/myhealth/main.tf
@@ -134,14 +134,3 @@ resource "azurerm_storage_table" "activityfileduplicate" {
   name = var.file_duplicate_table_name
   storage_account_name = data.azurerm_storage_account.velidastorage.name
 }
-
-# Create API Management
-resource "azurerm_api_management" "main" {
-  name = var.apim_name
-  location = var.resource_group_location
-  resource_group_name = var.resource_group_name
-  publisher_name = var.publisher_name
-  publisher_email = var.publisher_email
-
-  sku_name = "Consumption_0"
-}
