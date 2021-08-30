6423e6d4f8ae80b355c0cf9b6ad174f0706e055e
willvelida <willvelida@hotmail.co.uk>
2021-08-30 18:33:42 +1200

    Adding service for MyHealth.API.Exercise

---
+++ services/myhealth-api-exercise/variables.tf
@@ -0,0 +1,43 @@
+variable "resource_group_name" {
+  default = "myhealthapiexercise-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "velida_key_vault" {
+  default = "willvelidakeyvault"
+}
+
+variable "velida_resource_group_name" {
+  default = "velidaazureengine-rg"
+}
+
+variable "myhealth_app_service_plan" {
+  default = "myhealthappplan"
+  description = "Name of the App Service Plan for MyHealth"
+  type = string
+}
+
+variable "myhealth_app_resource_group" {
+  default = "myhealth-appserviceplan-rg"
+  description = "The name of the resource group for MyHealth"
+  type = string
+}
+
+variable "function_storage_name" {
+  default = "samyhealthapiexercise"
+  description = "Name of the storage account for MyHealth.API.Exercise"
+  type = string
+}
+
+variable "myhealth_api_exercise_function_name" {
+  default = "famyhealthapiexercise"
+  description = "Name of the MyHealth.API.Exercise App"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "e60bb76c-8fda-4ed4-a354-4836e3bfcbc3"
+}
