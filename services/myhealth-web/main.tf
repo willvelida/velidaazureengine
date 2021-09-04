8b799b3c70529eaabb5bc90c030290690bc6863b
willvelida <willvelida@hotmail.co.uk>
2021-09-04 16:33:39 +1200

    Adding service for MyHealth.Web

---
+++ services/myhealth-web/main.tf
@@ -0,0 +1,36 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "2.46.0"
+      }
+    }
+    backend "azure" {
+      resource_group_name = "velidarg"
+      storage_account_name = "velidaterraform"
+      container_name = "myhealthwebtfstate"
+      key = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+    source = "../../modules/resource_group"
+    resource_group_name = var.resource_group_name
+    resource_group_location = var.resource_group_location
+    resource_group_tags = {
+        "Terraform" = "true"
+        "ApplicationName" = "MyHealth"
+        "ServiceName" = "MyHealth.Web"
+    }  
+}
+
+resource "azurerm_static_site" "myhealthweb" {
+  name = var.website_name
+  resource_group_name = module.resource_group.name
+  location = module.resource_group.location
+  sku_size = var.sku_size
+}
---
+++ services/myhealth-web/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "myhealthweb-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "website_name" {
+    default = "myhealthweb"
+}
+
+variable "sku_size" {
+    default = "Free"
+}
