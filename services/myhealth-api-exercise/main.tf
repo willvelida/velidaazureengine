efb616782d186f8e829de2114dcf10748e9d4166
willvelida <willvelida@hotmail.co.uk>
2021-09-13 19:26:51 +1200

    Trying something

--- services/myhealth-api-exercise/main.tf
+++ services/myhealth-api-exercise/main.tf
@@ -5,12 +5,6 @@ terraform {
         version = "2.46.0"
       }
     }
-    backend "azure" {
-      resource_group_name = "velidarg"
-      storage_account_name = "velidaterraform"
-      container_name = "myhealthapiexercisetfstate"
-      key = "terraform.tfstate"
-    }
 }
 
 provider "azurerm" {
