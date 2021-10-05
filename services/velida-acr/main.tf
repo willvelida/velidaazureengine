88f92bc8c5c0ff786bd09566c7766e360eab784c
willvelida <willvelida@hotmail.co.uk>
2021-10-05 21:25:19 +1300

    Adding ARC service folder

---
+++ services/velida-acr/main.tf
@@ -0,0 +1,35 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "2.46.0"
+      }
+    }
+    backend "azure" {
+      resource_group_name = "velidarg"
+      storage_account_name = "velidaterraform"
+      container_name = "velidaacrtfstate"
+      key = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+    source = "../../modules/resource_group"
+    resource_group_name = var.resource_group_name
+    resource_group_location = var.resource_group_location
+    resource_group_tags = {
+        "Terraform" = "true"
+        "Resource-Specific" = "true"
+    }  
+}
+
+resource "azurerm_container_registry" "acr" {
+  name = var.velida_arc_name
+  resource_group_name = module.resource_group.name
+  location = module.resource_group.location
+  sku = "Basic"
+}
---
+++ services/velida-acr/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "velidaacr-rg"
+}
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "velida_arc_name" {
+    default = "velidaacr"
+}
