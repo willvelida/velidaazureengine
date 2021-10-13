cc80de2828652e71d60542220b0d41202f2d83b8
willvelida <willvelida@hotmail.co.uk>
2021-10-13 18:54:08 +1300

    Converting to Terraform for APIM

---
+++ services/myhealth-apim-management/main.tf
@@ -0,0 +1,40 @@
+terraform {
+    required_providers {
+      azurerm = {
+        source = "hashicorp/azurerm"
+        version = "2.46.0"
+      }
+    }
+    backend "azure" {
+      resource_group_name = "velidarg"
+      storage_account_name = "velidaterraform"
+      container_name = "myhealthapimtfstate"
+      key = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+    source = "../../modules/resource_group"
+    resource_group_name = var.resource_group_name
+    resource_group_location = var.resource_group_location
+    resource_group_tags = {
+        "Terraform" = "true"
+        "Resource-Specific" = "true"
+        "ApplicationName" = "MyHealth"
+
+    }  
+}
+
+resource "azurerm_api_management" "apim" {
+  name = var.myhealth_apim_name
+  location = module.resource_group.location
+  resource_group_name = module.resource_group.name
+  publisher_name = var.myhealth_publisher_name
+  publisher_email = var.myhealth_publisher_email
+  
+  sku_name = "Developer_1"
+}
---
+++ services/myhealth-apim-management/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "myhealthapim-rg"
+} 
+
+variable "resource_group_location" {
+  default = "australiaeast"
+}
+
+variable "myhealth_apim_name" {
+  default = "myhealthapim"
+}
+
+variable "myhealth_publisher_name" {
+  default = "Will Velida"
+}
+
+variable "myhealth_publisher_email" {
+  default = "willvelida@hotmail.co.uk"
+}
